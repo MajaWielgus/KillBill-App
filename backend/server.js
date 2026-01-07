7375a0b56ed4858690de03de87cb2a933a16ad9a
Maja <majawie@op.pl>
2026-01-07 18:23:57 +0100

    Połączenie z bazą danych

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-//do łączenia się z Reactem
+// do polaczenia z react
 app.use(cors());
 app.use(express.json());
 
-// testowy adres - czy działa
+// Polaczenie z baza danych MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Polaczono z baza danych MongoDB'))
+    .catch((err) => console.error('Blad polaczenia z baza:', err));
+
+// Testowy route
 app.get('/', (req, res) => {
-    res.send('Serwer KillBill działa');
+    res.send('Serwer dziala poprawnie');
 });
 
-// start serwera
+// Start serwera
 app.listen(PORT, () => {
-    console.log(`Serwer nasłuchuje na porcie: ${PORT}`);
+    console.log(`Serwer nasluchuje na porcie: ${PORT}`);
 });
